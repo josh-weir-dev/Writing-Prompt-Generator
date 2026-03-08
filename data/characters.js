1079a5405527e1b0c0ef6fbfc7f4148279b830eb
Josh Weir <Joshweir06@hotmail.com>
2026-03-08 21:31:12 +0000

    Added more options for fantasy settings and characters

--- data/characters.js
+++ data/characters.js
@@ -1,5 +1,6 @@
 export const characters = {
-    Fantasy: ["A Princess", "A Pirate", "A Knight", "A Mermaid"],
+    Fantasy: ["A Princess", "A Pirate", "A Knight", "A Mermaid", "A Fairy", "A Wizard", "A Servant"
+            ,"A Thief", "An Elf", "A Ruler", "A Normal Human"],
     "Sci-Fi": ["A Mad Scientist", "A Rich Investor", "An Astronaut", "An Alien"],
     Romance: ["An Average Human", "A Hidden Nobleperson", "A Global Sports Star"],
     Mystery: ["A Retired Police Officer", "An Intern detective", "A Criminal"]
--- data/settings.js
+++ data/settings.js
@@ -1,5 +1,6 @@
 export const settings = {
-    Fantasy: ["Enchanted Forest", "Floating City", "Castle"],
+    Fantasy: ["Enchanted Forest", "Floating City", "Castle", "A Rocky Mountain", "A Seaside Marketplace", 
+            "A Lost Island", "A Magic School"],
     "Sci-Fi": ["Space Station", "Distant Planet", "Labratory"],
     Romance: ["Small Village", "Coffee Shop", "University Campus"],
     Mystery: ["Quiet Seaside Town", "Moving Public Transport", "Abandoned Mansion"]
