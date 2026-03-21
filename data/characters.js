aa5d5de00a911db7010f91a84c8643a429f69476
Josh Weir <Joshweir06@hotmail.com>
2026-03-21 21:35:23 +0000

    Added new options for characters and settings

--- data/characters.js
+++ data/characters.js
@@ -1,6 +1,8 @@
 export const characters = {
     Fantasy: ["A Princess", "A Pirate", "A Knight", "A Mermaid", "A Fairy", "A Wizard", "A Servant"
-            ,"A Thief", "An Elf", "A Ruler", "A Normal Human"],
+            ,"A Thief", "An Elf", "A King", "A Normal Human","A Queen", "A Prince", "A Local Baker",
+            "A Merchant", "A Court Jester", "A Mysterious Hooded Figure", "A Witch", "A Student",
+            "An Angel", "A Demon", "A Con-Artist", "A Pixie", "A Vampire", "A God"],
     "Sci-Fi": ["A Mad Scientist", "A Rich Investor", "An Astronaut", "An Alien"],
     Romance: ["An Average Human", "A Hidden Nobleperson", "A Global Sports Star"],
     Mystery: ["A Retired Police Officer", "An Intern detective", "A Criminal"]
--- data/settings.js
+++ data/settings.js
@@ -1,7 +1,13 @@
 export const settings = {
-    Fantasy: ["Enchanted Forest", "Floating City", "Castle", "A Rocky Mountain", "A Seaside Marketplace", 
-            "A Lost Island", "A Magic School"],
-    "Sci-Fi": ["Space Station", "Distant Planet", "Labratory"],
-    Romance: ["Small Village", "Coffee Shop", "University Campus"],
+    Fantasy: ["Enchanted Forest", "Floating City", "Castle", "Rocky Mountain", "Seaside Marketplace", 
+            "Lost Island", "Magic School", "Ballroom", "Village Marketplace", "Ship on the Sea", 
+            "Abandoned Castle", "Overgrown Tower", "Fairy Garden", "Moorland", "Prairie", "Farm", 
+            "Underground Civilisation"],
+    "Sci-Fi": ["Space Station", "Cool Planet", "Labratory", "Underground Civillisation", "Space Ship",
+            "Warm Planet", "Aquatic Planet", "Overrun Planet", "Abandoned Planet" ],
+    Romance: ["Small Village", "Coffee Shop", "University Campus", "Supermarket", "Wedding", "Theatre"
+            ,"Lecture Hall", "Lake", "Party", "Award Ceremony", "Sports Competition", "Movie Set"
+            , "The White House", "Battlefield", "Disneyland", "Bookshop", "Bar/Pub", "Office", 
+            "New York", "London", "Madrid", "Beach", "Cinema", "Restaurant"],
     Mystery: ["Quiet Seaside Town", "Moving Public Transport", "Abandoned Mansion"]
 };
